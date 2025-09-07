2b513998259db8e5772a252ac05ee1b7c7e72feb
faseeh1279 <faseehraza1279@gmail.com>
2025-09-07 10:40:00 -0700

    ...

--- src/app/features/riders/riders.module.ts
+++ src/app/features/riders/riders.module.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'dashboard-page',
+    path: 'riders-page',
     component: RidersComponent
   }
 ];
